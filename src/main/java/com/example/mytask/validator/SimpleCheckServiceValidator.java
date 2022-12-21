e19ea00a747fabe001e374e6baf87b5a3b9a9a09
dangeonmaster <dafae722@gmail.com>
2022-12-21 22:09:47 +0300

    update check service validator

--- src/main/java/com/example/mytask/validator/SimpleCheckServiceValidator.java
+++ src/main/java/com/example/mytask/validator/SimpleCheckServiceValidator.java
@@ -1,8 +1,33 @@
 package com.example.mytask.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleCheckServiceValidator implements CheckServiceValidator {
+
+    public static final String REGEX_FIND_ONLY_NUMBERS = "^[0-9]{1,}$";
+
     @Override
     public boolean validate(String[] productsId, String[] countProductsOnEachId, String discountCardId) {
-        return productsId != null && countProductsOnEachId != null;
+        if (productsId == null || countProductsOnEachId == null) {
+            return false;
+        }
+        if (!validateArraysStringData(productsId) || !validateArraysStringData(countProductsOnEachId)) return false;
+        return true;
+    }
+
+    private static boolean validateArraysStringData(String[] productsId) {
+        Pattern pattern = Pattern.compile(REGEX_FIND_ONLY_NUMBERS);
+        for (String data : productsId) {
+            if (data == null) {
+                return false;
+            } else {
+                Matcher matcher = pattern.matcher(data);
+                if (!matcher.find()) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
